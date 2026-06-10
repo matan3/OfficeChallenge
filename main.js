63e1a27b5d571e7f8af3d734822ca28973282d35
Matan <matan4491@gmail.com>
2026-06-11 00:01:20 +0300

    implement Document, Employee, Manager, Cleaner, and Office classes with work and hiring functionalities

--- main.js
+++ main.js
@@ -1,2 +1,57 @@
-//write your code here to make the tests pass
+class Document {
+    constructor(EmployeeName) {
+        this.EmployeeName = EmployeeName;
+    }
+}
+
+class Employee {
+    constructor(name) {
+        this.name = name;
+    }
+    work(office) {
+        for (let i = 0; i < 10; i++) {
+            office.documents.push(new Document(this.name));
+        }
+
+    }
+}
+
+class Manager {
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    }
+    hireEmployee(name) {
+        this.employees.push(new Employee(name));
+    }
+    askEmployeesToWork(office) {
+        this.employees.forEach(employee => employee.work(office));
+    }
+}
+
+class Cleaner {
+    constructor(name) {
+        this.name = name;
+    }
+    clean() {
+        console.log("Clean");
+    }
+}
+
+class Office {
+    constructor(documents) {
+        this.documents = [];
+        this.managers = [];
+        this.cleaners = [];
+    }
+    hireManager(name) {
+        this.managers.push(new Manager(name));
+    }
+    hireCleaner(name) {
+        this.cleaners.push(new Cleaner(name));
+    }
+    startWorkDay() {
+        this.managers.forEach(manager => manager.askEmployeesToWork(this));
+    }
+}
 
